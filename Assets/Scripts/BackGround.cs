d6aeb40d3e61b2da2b7a6bdd12e3ad36af511231
Kieran <kieranc.inbox@gmail.com>
2021-06-17 23:49:01 +1000

    Background done

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense.Background
+{
+    public class BackGround : MonoBehaviour
+    {
+        public float speed = 100; 
+
+        // Update is called once per frame
+        // This will constantly move it and then move it to the back
+        void Update()
+        {
+            transform.position += new Vector3((speed * Time.deltaTime),0,0);
+            print(transform.position);
+            if (transform.position.x>=2000)
+            {
+                transform.position += new Vector3(-3000, 0, 0);
+            }
+        }
+    }
+}
